b29b4b03f7ddd58bb30ad8d2dcad4a63affae39b
Anirban <anirbansjob27@gmail.com>
2016-04-04 10:32:26 +0800

    Distance calculation added

--- app/src/main/java/com/ibraheem/ibraheem/MainActivity.java
+++ app/src/main/java/com/ibraheem/ibraheem/MainActivity.java
@@ -1,11 +1,16 @@
 package com.ibraheem.ibraheem;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 import com.ibraheem.ibraheem.widget.MyPagerAdapter;
+
+import java.util.Calendar;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -28,21 +33,21 @@ public class MainActivity extends AppCompatActivity {
         myPagerAdapter = new MyPagerAdapter(getSupportFragmentManager());
         viewPager.setAdapter(myPagerAdapter);
         tabLayout.setupWithViewPager(viewPager);
-        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-                    
-            }
 
-            @Override
-            public void onPageSelected(int position) {
 
-            }
+        //========================================================================================
+        // ++++++++++++++++++++++++++++++ Start Tracker service ++++++++++++++++++++++++++++++++++
+        //========================================================================================
 
-            @Override
-            public void onPageScrollStateChanged(int state) {
+        Intent myIntent1 = new Intent(this, MyMainReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, myIntent1, 0);
 
-            }
-        });
+        AlarmManager alarmManager1 = (AlarmManager) getSystemService(ALARM_SERVICE);
+        long recurring = (10 * 1000);  // in milliseconds
+        alarmManager1.setRepeating(AlarmManager.RTC_WAKEUP, Calendar.getInstance().getTimeInMillis(), recurring, pendingIntent);
+
+        //========================================================================================
+        // ++++++++++++++++++++++++++++++ Start Tracker service ++++++++++++++++++++++++++++++++++
+        //========================================================================================
     }
 }
